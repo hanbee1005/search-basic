75f30b02affd46958a20ff6bc9847f2cbab8b498
hanbee <sonhanbi1002@gmail.com>
2024-01-17 07:00:47 +0900

    feat: 테스트 프로파일 추가

--- src/test/java/com/example/searchbasic/SearchServiceIntegrationTest.java
+++ src/test/java/com/example/searchbasic/SearchServiceIntegrationTest.java
@@ -5,10 +5,14 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.concurrent.CompletableFuture;
 
+@ActiveProfiles("test")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @SpringBootTest
 class SearchServiceIntegrationTest {
     @Autowired
